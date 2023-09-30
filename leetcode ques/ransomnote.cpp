ec8a4fedf7e191141b7346d3d3a58715c2dd2373
adityaaa2511 <137895529+adityaaa2511@users.noreply.github.com>
2023-09-30 18:31:09 +0530

    Add files via upload

---
+++ leetcode ques/ransomnote.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int arr1[26]={0},arr2[26]={0};
+        for(int i=0;i<ransomNote.size();i++){
+            arr1[ransomNote[i]-'a']++;
+        }
+        for(int i=0;i<magazine.size();i++){
+            arr2[magazine[i]-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(arr1[i]>arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+//APP 2
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int arr1[26]={0},arr2[26]={0};
+        for(int i=0;i<ransomNote.size();i++){
+            arr1[ransomNote[i]-'a']++;
+        }
+        for(int i=0;i<magazine.size();i++){
+            arr2[magazine[i]-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(arr1[i]>arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
