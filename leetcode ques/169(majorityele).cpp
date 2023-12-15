4b7da807df2331683d7f31d1a59b22e958d74d80
Aditya Saxena <137895529+adityaaa2511@users.noreply.github.com>
2023-12-15 22:37:30 +0530

    Add files via upload

---
+++ leetcode ques/169(majorityele).cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>m;
+        for(int i=0;i<n;i++){
+            if(!m.count(nums[i])){
+                m[nums[i]]=1;
+            }
+            else{
+                m[nums[i]]++;
+            }
+        }
+        int val;
+        for(auto x:m){
+            if(x.second>(n/2)){
+                val=x.first;
+                break;
+            }
+        }
+        return val;
+    }
+};
